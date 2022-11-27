2cec0776eff8673b96eb6620fec2c8fe2531b726
Morris Alper <morrisalp@gmail.com>
2022-11-27 19:38:41 +0200

    update

--- launch_jl.sh
+++ launch_jl.sh
@@ -3,7 +3,7 @@
 if [ -n "$KERNEL_ENVS_DIR" ]
 then
 
-    if [-n "$CLONE_BASE_TO" ]
+    if [ -n "$CLONE_BASE_TO" ]
     then
         echo "Cloning base conda environment into: $CLONE_BASE_TO (within $KERNEL_ENVS_DIR)"
         conda create --prefix "$KERNEL_ENVS_DIR/$CLONE_BASE_TO" --clone base
